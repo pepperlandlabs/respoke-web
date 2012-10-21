9eb9bb5036d05cf5169722ed98da647fb357ee93
Edmund Salvacion <edmund@edmundatwork.com>
2012-10-21 13:45:34 -0700

    [CHORE] First commitsies of respoke-web

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RespokeWeb::Application.config.secret_token = '49b4753e11ed874ab37028465bebd02e237e93d49993c5338b9ca977100c2294c6b8740e87f22b748db4f66316cea2af4cf35c6aea0063a52f12d812e9c4b801'
